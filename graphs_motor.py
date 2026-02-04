f89a15c505dbd8f6befa72ff8e84275478d6c894
Andrick Serrano <andrick.serrano@tamu.edu>
2026-02-04 19:46:19 +0000

    graphs_motor looks good to go

---
+++ graphs_motor.py
@@ -0,0 +1,30 @@
+### TESTING CODE ###
+
+#basci imports needed for plotting
+import numpy as np
+import matplotlib.pyplot as plt
+from subfunctions import tau_dcmotor, motor
+
+omega = np.linspace(0.0, motor["speed_noload"], 500)
+tau = tau_dcmotor(omega, motor)
+P = tau * omega
+
+plt.figure()
+
+plt.subplot(3,1,1)
+plt.plot(tau, omega)
+plt.xlabel("Motor Shaft Torque [Nm]")
+plt.ylabel("Motor Shaft Speed [rad/s]")
+
+plt.subplot(3,1,2)
+plt.plot(tau, P)
+plt.xlabel("Motor Shaft Torque [Nm]")
+plt.ylabel("Motor Power [W]")
+
+plt.subplot(3,1,3)
+plt.plot(omega, P)
+plt.xlabel("Motor Shaft Speed [rad/s]")
+plt.ylabel("Motor Power [W]")
+
+plt.tight_layout()
+plt.show()
